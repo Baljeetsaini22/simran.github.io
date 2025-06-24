cc282ad95fe48e4016f25ac0a279a3ea0be6aee3
Baljeetsaini22 <baljeetsaini7440@gmail.com>
2025-06-24 15:02:42 +0530

    dcv

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -7,7 +7,7 @@ import logo1 from "/logo/digibeez.svg";
 
 const experienceData = [
   {
-    year: "Form Nov 2024",
+    year: "2024 - Present",
     logo: logo1,
     role: "UI/UX Designer",
     company: "Digibeez",
@@ -18,39 +18,39 @@ const experienceData = [
 
 const Experience = () => {
   return (
-    <section id="experience" className="bg-black text-white py-16 px-4 md:px-[7.5%]">
-      <div className="max-w-7xl mx-auto">
+    <section id="experience" className="bg-black text-white w-full h-auto px-[7.5%] py-16">
+      <div>
         <h2 className="text-3xl font-bold text-center mb-12 text-[#ffe600]">
           My Work Experience
         </h2>
 
-        <div className="space-y-6">
+        <div className="space-y-6 flex items-center">
           {experienceData.map((exp, idx) => (
             <div
               key={idx}
-              className={`flex flex-col md:flex-row items-center justify-between bg-gray-900 p-6 rounded-xl shadow-[0_-5px_5px] ${exp.shadow} hover:shadow-[0_5px_5px] hover:${exp.shadow} transform hover:scale-105 transition-all duration-300`}
+              className={`w-full grid items-center md:grid-cols-2 lg:grid-cols-4 text-center gap-5 bg-gray-900 p-6 rounded-xl shadow-[0_-5px_5px] ${exp.shadow} hover:shadow-[0_5px_5px] hover:${exp.shadow} transform hover:scale-105 transition-all duration-300`}
             >
               {/* Left: Year */}
-              <div className="text-gray-300 font-semibold text-sm md:text-base w-full md:w-[15%] mb-4 md:mb-0">
+              <div className="text-gray-300 font-semibold text-sm md:text-base">
                 {exp.year}
               </div>
 
               {/* Middle: Logo */}
-              <div className="flex-shrink-0 rounded-2xl overflow-hidden mb-4 md:mb-0">
+              <div className="overflow-hidden flex justify-center">
                 <img src={exp.logo} alt="company logo" className="object-contain w-14 h-full" />
               </div>
 
               {/* Right: Info */}
-              <div className=" md:text-left">
+              <div className="">
                 <h3 className="text-lg font-bold text-white">{exp.role}</h3>
                 <p className="text-sm text-red-400">{exp.company}</p>
               </div>
 
               {/* Website link */}
-              <div className="mt-4 md:mt-0">
+              <div className="text-center flex justify-center">
                 <a
                   href={exp.website}
-                  className="text-white hover:text-[#ffe600] text-sm font-semibold flex items-center gap-2"
+                  className="text-white hover:text-[#ffe600] text-sm font-semibold flex items-center  gap-2"
                 >
                   Go to website <FaArrowRight />
                 </a>
